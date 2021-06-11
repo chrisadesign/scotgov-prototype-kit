ea3fce8b6033f1674dea28dfa76e960702c5cc06
Chris Armstrong <mail@chrisarmstrong.io>
2021-06-11 12:27:53 +0100

    Added scotgov pattern lib.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,3 @@
-/* global $ */
-
-// Warn about using the kit in production
-if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
-}
-
-$(document).ready(function () {
-  window.GOVUKFrontend.initAll()
-})
+// $(document).ready(function () {
+//   window.DS.initAll()
+// })
